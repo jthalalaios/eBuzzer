5803d076255915478fe33540570cc1c437aa58f1
jthalalaios <48445852+jthalalaios@users.noreply.github.com>
2021-06-14 20:09:52 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,12 @@
 <?php
-define("MYSQLHOST", ""); //location of mysql.sock
-define("MYSQLUSER", "");  //mysql user => root
-define("MYSQLPASS", ""); //mysql pass
-define("MYSQLPREF", "");	//"prt"
-define("MYSQLBASE", "");	//database name , "ebuzzer"
+define("MYSQLHOST", "");  //location of mysql.sock
+define("MYSQLUSER", "");  //mysql user  "root"
+define("MYSQLPASS", "");  //mysql pass "mypass"
+define("MYSQLPREF", "");  //"prt"
+define("MYSQLBASE", "");  //database name , "ebuzzer"
 define("MYSQLHTYP", "");	//unix_socket
-define("HTMLHOME", ""); //home path of ur html for example user.gr/~this_user/
+define("HTMLHOME", "");   //home path of ur html for example user.com/~this_user/
 define("DISPERROR", "1");
 define("HELPON", TRUE);
-define("MYSQLPORT", ""); //mysql port for connection
+define("MYSQLPORT", "");  //mysql port for connection
 ?>
