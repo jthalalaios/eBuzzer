73cc1c336175e4cd2050b235c6092f0428bcfb5b
jthalalaios <48445852+jthalalaios@users.noreply.github.com>
2021-06-14 16:54:42 +0300

    Add files via upload

---
+++ includes/js/getValueProduct.js
@@ -0,0 +1,9 @@
+<script>
+function setHidden2(product_name)
+{
+    var element = document.getElementById("product_id");
+    element.value = product_name;
+    return true;
+}
+</script>
+
---
+++ includes/js/logout-start.js
@@ -0,0 +1,10 @@
+<script>
+function logout() {
+  window.location.href = '../includes/logout.php'
+}
+</script>
+<script>
+function start() {
+  window.location.href = 'index.php'
+}
+</script>
