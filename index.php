e75f777d537806ea4a27cbe2eb9566bb7cf292aa
jthalalaios <48445852+jthalalaios@users.noreply.github.com>
2021-06-14 14:52:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- 
+access = Everyone
+purpose = the main page -->
+<?php 
+require_once ('includes/header.php'); 
+?>
+<?php 
+ require_once ('includes/slider.php');    
+ require_once('includes/footer.php'); 
+ ?>
+</body>
+</html>
