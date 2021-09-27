112131a0e6267ada23005bf1afc6300fa9f7c8c3
jthalalaios <48445852+jthalalaios@users.noreply.github.com>
2021-09-27 12:27:13 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,12 @@
 <?php
-define("MYSQLHOST", "/zstorage/home/ece00592/mysql/run/mysql.sock");
-define("MYSQLUSER", "root");
-define("MYSQLPASS", "123div321");
-define("MYSQLPREF", "prt");
-define("MYSQLBASE", "ebuzzer");
-define("MYSQLHTYP", "unix_socket");
-define("HTMLHOME", "zafora.icte.uowm.gr/~ece00592/");
+define("MYSQLHOST", "");  //location of mysql.sock
+define("MYSQLUSER", "");  //mysql user  "root"
+define("MYSQLPASS", "");  //mysql pass "mypass"
+define("MYSQLPREF", "prt");  //"prt"
+define("MYSQLBASE", "");  //database name , "ebuzzer"
+define("MYSQLHTYP", "unix_socket");	//unix_socket
+define("HTMLHOME", "");   //home path of ur html for example user.com/~this_user/
 define("DISPERROR", "1");
 define("HELPON", TRUE);
-define("MYSQLPORT", "3306");
+define("MYSQLPORT", "");  //mysql port for connection
 ?>
