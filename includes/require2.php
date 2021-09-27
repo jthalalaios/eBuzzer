91484bbc8d6097a59d1b42502ddeef5445a9bc1e
jthalalaios <48445852+jthalalaios@users.noreply.github.com>
2021-09-27 17:59:56 +0300

    Update require2.php

--- includes/require2.php
+++ includes/require2.php
@@ -15,3 +15,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/js/all.min.js"> </script>
 
+  </head>
+</html>
